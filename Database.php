badc5da5dced9bcb929f6218578aee5bfde6b418
Yarden <yarden.paniry@gmail.com>
2022-06-06 19:53:27 +0300

    define database class

--- Database.php
+++ Database.php
@@ -22,21 +22,61 @@ class Database
       return false;
     }
   }
-  public function select($table, $row = "*", $where = null, $order = null){
-
+  public function select($table, $row = "*", $where = null, $order = null)
+  {
+    $query = 'SELECT ' . $row . ' FROM ' . $table;
+    if ($where != null) {
+      $query .= ' WHERE ' . $where;
+    }
+    if ($order != null) {
+      $query .= ' ORDER BY ';
+    }
+    $Result = $this->DbCon->query($query);
+    return $Result;
   }
   public function insert($table, $value, $row = null)
   {
-
+    $insert = " INSERT INTO " . $table;
+    if ($row != null) {
+      $insert .= " (" . $row . " ) ";
+    }
+    for ($i = 0; $i < count($value); $i++) {
+      if (is_string($value[$i])) {
+        $value[$i] = '"' . $value[$i] . '"';
+      }
+    }
+    $value = implode(',', $value);
+    $insert .= ' VALUES (' . $value . ')';
+    $ins = $this->DbCon->query($insert);
+    if ($ins) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public function delete($table, $where = null)
   {
-   
+    if ($where == null) {
+      $delete = "DELETE " . $table;
+    } else {
+      $delete = "DELETE  FROM " . $table . " WHERE " . $where;
+    }
+    $del = $this->DbCon->query($delete);
+    if ($del) {
+      return true;
+    } else {
+      return false;
+    }
   }
   public function update($table, $rows, $where)
   {
-   
+    $update = 'UPDATE ' . $table . ' SET ' . $rows . ' WHERE ' . $where;
+    $query = $this->DbCon->query($update);
+    if ($query) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
 }
